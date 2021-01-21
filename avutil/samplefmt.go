855059af2adac503294de9d6fb49e886f505f6ba
Tsiry Sandratraina <tsiry.sndr@gmail.com>
2021-01-21 23:26:19 +0300

    avutil: add samplefmt constants

--- avutil/samplefmt.go
+++ avutil/samplefmt.go
@@ -8,6 +8,22 @@ type (
 	AvSampleFormat C.enum_AVSampleFormat
 )
 
+const (
+	AV_SAMPLE_FMT_NONE = C.AV_SAMPLE_FMT_NONE
+	AV_SAMPLE_FMT_U8   = C.AV_SAMPLE_FMT_U8  ///< unsigned 8 bits
+	AV_SAMPLE_FMT_S16  = C.AV_SAMPLE_FMT_S16 ///< signed 16 bits
+	AV_SAMPLE_FMT_S32  = C.AV_SAMPLE_FMT_S32 ///< signed 32 bits
+	AV_SAMPLE_FMT_FLT  = C.AV_SAMPLE_FMT_FLT ///< float
+	AV_SAMPLE_FMT_DBL  = C.AV_SAMPLE_FMT_DBL ///< double
+
+	AV_SAMPLE_FMT_U8P  = C.AV_SAMPLE_FMT_U8P  ///< unsigned 8 bits, planar
+	AV_SAMPLE_FMT_S16P = C.AV_SAMPLE_FMT_S16P ///< signed 16 bits, planar
+	AV_SAMPLE_FMT_S32P = C.AV_SAMPLE_FMT_S32P ///< signed 32 bits, planar
+	AV_SAMPLE_FMT_FLTP = C.AV_SAMPLE_FMT_FLTP ///< float, planar
+	AV_SAMPLE_FMT_DBLP = C.AV_SAMPLE_FMT_DBLP ///< double, planar
+	AV_SAMPLE_FMT_NB   = C.AV_SAMPLE_FMT_NB
+)
+
 func AvSampleFormatFmtIsPlanar(sf AvSampleFormat) int {
 	return int(C.av_sample_fmt_is_planar((C.enum_AVSampleFormat)(sf)))
 }
